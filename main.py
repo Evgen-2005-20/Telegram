4b26f6ff2d949bdd51c035d043fd68c9bafcb137
Evgen-2005-20 <jena1245481te@stud.op.edu.ua>
2026-02-24 13:00:58 +0000

    bots main completed

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import asyncio
 
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+
+from bot.config import settings
+from bot.db.db import rd
+
+
+async def main():
+    bot = Bot(token=settings.TOKEN)
+    redis = RedisStorage(redis=rd)
+    dp = Dispatcher(storage=redis)
+    
+    await dp.start_polling(bot)
+    
+    
+if __name__ == "__main__":
+    asyncio.run(main())
 
