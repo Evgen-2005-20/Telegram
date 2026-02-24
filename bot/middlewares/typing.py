164d3fdc206eaaf52b2b2c881721c8f8332a6c25
Evgen-2005-20 <jena1245481te@stud.op.edu.ua>
2026-02-24 15:41:32 +0000

    Middleware was updated

--- bot/middlewares/typing.py
+++ bot/middlewares/typing.py
@@ -1,19 +1,26 @@
 import asyncio
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.enums import ChatAction
 
 
-class TypingMessage(BaseMiddleware):
+class TypingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
-        if isinstance(event, Message):
-            bot = data["bot"]
+        bot = data["bot"]
 
+        chat_id = None
+
+        if isinstance(event, Message):
+            chat_id = event.chat.id
+
+        elif isinstance(event, CallbackQuery):
+            chat_id = event.message.chat.id
+
+        if chat_id:
             await bot.send_chat_action(
-                chat_id=event.chat.id,
+                chat_id=chat_id,
                 action=ChatAction.TYPING
             )
-
             await asyncio.sleep(2)
 
         return await handler(event, data)
